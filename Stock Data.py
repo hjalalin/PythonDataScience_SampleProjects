bc2ada1a38ff437342fb79106d3ef29fd71dbaf6
Hoda Jalali <hoj14@pitt.edu>
2022-05-09 14:54:24 -0400

    Add files via upload

---
+++ Stock Data.py
@@ -0,0 +1,36 @@
+!pip install yfinance==0.1.67
+#!pip install pandas==1.3.3
+#!pip install requests==2.26.0
+!mamba install bs4==4.10.0 -y
+#!pip install plotly==5.3.1
+
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+tesla = yf.Ticker("TSLA")
+tesla_data = tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+tesla_data.head()
+
+html_data  = requests.get("https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue").text
+soup = BeautifulSoup(html_data, 'html5lib')
+
+tesla_revenue = pd.DataFrame(columns=["Date", "Revenue"])
+
+for row in soup.find_all("tbody")[1].find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text
+
+    tesla_revenue = tesla_revenue.append({"Date": date, "Revenue": revenue}, ignore_index=True)
+
+tesla_revenue["Revenue"] = tesla_revenue['Revenue'].str.replace(',|\$', "")
+
+tesla_revenue.dropna(inplace=True)
+
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+tesla_revenue.tail()
